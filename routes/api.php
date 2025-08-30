b2788929c2c18bd7e7c53cf537c052a1c78ace6b
mohamed khounti <larbikhounti@gmail.com>
2025-08-30 16:42:23 +0100

    Update API routes to include tenant creation endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\TenantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/tenant', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+       Route::post('/tenants', [TenantController::class, 'create']);
+    });
+}
