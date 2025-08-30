909238f0e827486063e1485fd4827d662bd6c0da
mohamed khounti <larbikhounti@gmail.com>
2025-08-30 16:41:36 +0100

    Implement create method in TenantController for tenant creation

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\services\TenantService;
 use Illuminate\Http\Request;
 
 class TenantController extends Controller
 {
-    //
+    protected $tenantService;
+
+    public function __construct(TenantService $tenantService)
+    {
+        $this->tenantService = $tenantService;
+    }
+
+    public function create(Request $request)
+    {
+        // Validate and create the tenant
+        $data = $request->validate([
+            'id' => 'required|string|max:255',
+            'name' => 'required|string|max:255',
+             'email' => 'required|string|email|max:255',
+        ]);
+       //dd($data);
+
+        $tenant = $this->tenantService->createTenant($data);
+
+      return response()->json($tenant, 201);
+    }
 }
