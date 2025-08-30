a364e5bf23a4285de994a146962474dfef513815
mohamed khounti <larbikhounti@gmail.com>
2025-08-30 17:28:13 +0100

    Update route to use CompanyController for tenant creation

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,12 @@
 <?php
 
-use App\Http\Controllers\TenantController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/tenant', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 
 
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
-       Route::post('/tenants', [TenantController::class, 'create']);
+        Route::post('/tenants', [CompanyController::class, 'create']);
     });
 }
