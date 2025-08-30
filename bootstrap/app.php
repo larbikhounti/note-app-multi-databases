84bc2925d1e02750100951df9cdc54f728986099
mohamed khounti <larbikhounti@gmail.com>
2025-08-30 19:47:40 +0100

    Comment out AuthMiddleware references in app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Middleware\AuthMiddleware;
+//use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(AuthMiddleware::class);
+       // $middleware->append(AuthMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
