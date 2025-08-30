4c8fb3e07bec4251c1906ad178a536165beea145
mohamed khounti <larbikhounti@gmail.com>
2025-08-30 16:42:31 +0100

    Remove default welcome route and implement domain-based routing for central domains

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/hello', function () {
+            return 'Hello, World!';
+        });
+    });
+}
