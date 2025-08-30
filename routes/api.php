99dec4ff6897575ed66e37f700f4bff093875b24
mohamed khounti <larbikhounti@gmail.com>
2025-08-30 17:29:44 +0100

    Update route to create companies in the correct tenant context

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
 
-
+// this insuring that the company is created in the correct tenant context (central domain)
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
-        Route::post('/tenants', [CompanyController::class, 'create']);
+        Route::post('/companies', [CompanyController::class, 'create']);
     });
 }
