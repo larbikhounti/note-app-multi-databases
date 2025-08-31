0d85eb28be950545114dd90ef2b8964a4c70cdff
mohamed khounti <larbikhounti@gmail.com>
2025-08-31 16:26:49 +0100

    fix: remove unused React import in Notes component

--- frontend/app/notes/page.tsx
+++ frontend/app/notes/page.tsx
@@ -2,12 +2,13 @@
 
 import { DataTable, Note } from "@/components/ui/data-table"
 import { fetcher } from "@/utils/axios"
-import * as React from "react"
 import useSWR from "swr"
+
 export default function Notes() {
     const { data, error } = useSWR<Note[]>("/notes", fetcher)
     if (error) return <div>Failed to load notes</div>
     if (!data) return <div>Loading...</div>
+    
   return (
      <div className="container mx-auto py-10">
       <DataTable  data={data} />
