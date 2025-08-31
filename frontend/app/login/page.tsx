5c7250bc1de7b3fa6c4d3af53cb03e320d55965b
mohamed khounti <larbikhounti@gmail.com>
2025-08-31 15:40:43 +0100

    feat: enhance LoginPage with token handling and error display

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,13 +3,15 @@
 import { LoginForm } from "@/components/ui/login-form"
 import { LoginFormData } from "@/types/login.type";
 import { poster } from "@/utils/axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 
 
-
-export default function Page() {
+export default function LoginPage() {
+  const router = useRouter();
     const [error, setError] = useState<string | null>(null);
+    const [token, setToken] = useState<string | null>(null);
 
      const handleSubmit = async (data: LoginFormData) => {
       setError(null);
@@ -19,18 +21,31 @@ export default function Page() {
       console.log(response);
       console.log(response.status);
 
-      if (response.status !== 201) {
+      if (response.status !== 200) {
         console.log(response.error);
         setError(response.data.error);
         return
       }
-      //setCompanyName(response.data.name);
+      setToken(response.data.token);
+
   }
 
+    useEffect(() => {
+      if (token) {
+        console.log("Setting token in local storage: " + token);
+        localStorage.setItem('token', token);
+        const storageToken = localStorage.getItem('token')
+        if (storageToken) {
+          router.push("/notes");
+        }
+
+      }
+    }, [token]);
 
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        {error && <div className="mb-4 text-red-600">{error}</div>}
         <LoginForm onSubmit={handleSubmit} />
       </div>
     </div>
