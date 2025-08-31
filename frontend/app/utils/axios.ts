d562a680f94d77e7e20305ca4492e3cc1b0b8af7
mohamed khounti <larbikhounti@gmail.com>
2025-08-31 15:40:10 +0100

    fix: correct axios instance creation and update fetcher response handling

--- frontend/app/utils/axios.ts
+++ frontend/app/utils/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 const createDynamicInstance = () => {
     if (typeof window !== 'undefined') {
         // Get current hostname including subdomain
@@ -7,7 +8,10 @@ const createDynamicInstance = () => {
         const protocol = window.location.protocol;
         
         return axios.create({
-            baseURL: `${protocol}//${currentHost}/api`
+            baseURL: `${protocol}//${currentHost}/api`,
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
         });
     }
     
@@ -22,7 +26,7 @@ export const getInstance = () => createDynamicInstance();
 console.log(getInstance());
 
 // Or create functions that use dynamic instance
-const fetcher = (url: string) => getInstance().get(url).then(res => res).catch(err => err.response.data)
+const fetcher = (url: string) => getInstance().get(url).then(res => res.data).catch(err => err.response.data)
 const poster = async (url: string, data: any) => getInstance().post(url, data).then(res => res).catch(err => err.response.data)
 const putter = (url: string, data: any) => getInstance().put(url, data).then(res => res).catch(err => err.response.data)
 const deleter = (url: string) => getInstance().delete(url).then(res => res).catch(err => err.response.data)
