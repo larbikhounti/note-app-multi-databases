7d8842dd631481f2af0128fb98328e0fed21d8db
mohamed khounti <larbikhounti@gmail.com>
2025-08-31 15:40:57 +0100

    feat: add human-readable timestamps for created and updated notes

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
@@ -15,5 +16,15 @@ class Note extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->diffForHumans();
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->diffForHumans();
     }
 }
